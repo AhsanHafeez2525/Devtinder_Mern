a0260ce003a922aaa534d0c60f60263d71758e84
Ahsan Hafeez Satti <ahsansatti402@gmail.com>
2025-07-28 22:32:37 +0500

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -8,15 +8,40 @@ const port = 3000;
 // app.use("/react", (req, res) => {
 //   res.send("Hello react");
 // });
-app.get("/user", (req, res) => {
-  res.send({ firstName: "ahsan", lastName: "satti" });
-});
-app.post("/user", (req, res) => {
-  res.send("data is save successfully into the database");
-});
-app.use("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/user", (req, res) => {
+//   res.send({ firstName: "ahsan", lastName: "satti" });
+// });
+// app.post("/user", (req, res) => {
+//   res.send("data is save successfully into the database");
+// });
+// app.use("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// middlware and route handler
+
+// app.use("/user", (req, res) => {
+// route handler
+// res.send("Route handler 1");
+// console.log("call Route handler 1");
+// });
+
+// multiple route handlers
+
+app.use(
+  "/user",
+  (req, res, next) => {
+    // route handler
+    res.send("Route handler 1");
+    console.log("call Route handler 1");
+    next();
+  },
+  (req, res) => {
+    // route handler 2
+    res.send("Route handler 2");
+    console.log("call Route handler 2");
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
