c211010fb1911f0d877e2de3fc6f0e6778a70739
Ahsan Hafeez Satti <ahsansatti402@gmail.com>
2025-07-28 21:54:39 +0500

    routing and request handler

--- src/app.js
+++ src/app.js
@@ -2,7 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+// app.use("/ahsan", (req, res) => {
+//   res.send("ahsan satti");
+// });
+// app.use("/react", (req, res) => {
+//   res.send("Hello react");
+// });
+app.get("/user", (req, res) => {
+  res.send({ firstName: "ahsan", lastName: "satti" });
+});
+app.post("/user", (req, res) => {
+  res.send("data is save successfully into the database");
+});
+app.use("/", (req, res) => {
   res.send("Hello World!");
 });
 
