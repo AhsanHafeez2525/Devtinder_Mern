3d1ecaafaec45ea385b8edf4a06d9600e04d32a9
Ahsan Hafeez Satti <ahsansatti402@gmail.com>
2025-08-05 23:36:54 +0500

    get apis

--- src/app.js
+++ src/app.js
@@ -1,9 +1,56 @@
+// const express = require("express");
+// const connectDB = require("./config/database");
+// const app = express();
+// const User = require("./models/user");
+// const port = 3000;
+// app.use(express.json());
+// app.post("/signup", async (req, res) => {
+//   console.log(req.body);
+//   const user = new User(req.body);
+//   try {
+//     await user.save();
+//     res.send("User added");
+//   } catch (err) {
+//     res.status(400).send("Error saving the user" + err.message);
+//   }
+
+//   // get user by email
+
+//   app.get("/user", async (req, res) => {
+//     const userEmail = req.body.emailId;
+//     try {
+//       const user = await User.find({ emailId: userEmail });
+//       res.send(user);
+//     } catch (err) {
+//       res.status(400).send("something went wrong");
+//     }
+//   });
+
+//   // Feed API - GET /feed - get all the users from the database
+
+//   // app.get("/feed", (req, res) => {});
+// });
+
+// connectDB()
+//   .then(() => {
+//     console.log("database connected successfully");
+//     app.listen(port, () => {
+//       console.log(`Example app listening on port ${port}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("database cannot be connected");
+//   });
+
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 const port = 3000;
+
 app.use(express.json());
+
+// ✅ POST /signup - Create new user
 app.post("/signup", async (req, res) => {
   console.log(req.body);
   const user = new User(req.body);
@@ -11,85 +58,45 @@ app.post("/signup", async (req, res) => {
     await user.save();
     res.send("User added");
   } catch (err) {
-    res.status(400).send("Error saving the user" + err.message);
+    res.status(400).send("Error saving the user: " + err.message);
   }
-  // creating a new instance of the user modal
-  // const user = new User({
-  //   firstName: "sayyam",
-  //   lastName: "satti",
-  //   emailId: "sayyamsatti@gmail.com",
-  //   password: "sayyam123",
-  // });
-  // try {
-  //   await user.save();
-  //   res.send("User added");
-  // } catch (err) {
-  //   res.status(400).send("Error saving the user" + err.message);
-  // }
 });
 
+// ✅ GET /user - Find user by email (pass emailId as query param)
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId; // Use query for GET
+  try {
+    // const user = await User.find({ emailId: userEmail });
+    const user = await User.findOne({ emailId: userEmail });
+    if (user.length == 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Something went wrong: " + err.message);
+  }
+});
+
+// ✅ Optional: GET /feed - Get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send("Failed to fetch users: " + err.message);
+  }
+});
+
+// ✅ Start the server
 connectDB()
   .then(() => {
-    console.log("database connected successfully");
+    console.log("Database connected successfully");
     app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
+      console.log(`Server listening on port ${port}`);
     });
   })
   .catch((err) => {
-    console.log("database cannot be connected");
+    console.log("Database connection failed:", err);
   });
-
-// app.use("/ahsan", (req, res) => {
-//   res.send("ahsan satti");
-// });
-// app.use("/react", (req, res) => {
-//   res.send("Hello react");
-// });
-// app.get("/user", (req, res) => {
-//   res.send({ firstName: "ahsan", lastName: "satti" });
-// });
-// app.post("/user", (req, res) => {
-//   res.send("data is save successfully into the database");
-// });
-// app.use("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
-// middlware and route handler
-
-// app.use("/user", (req, res) => {
-// route handler
-// res.send("Route handler 1");
-// console.log("call Route handler 1");
-// });
-
-// multiple route handlers
-
-// app.use(
-//   "/user",
-//   (req, res, next) => {
-//     // route handler
-//     res.send("Route handler 1");
-//     console.log("call Route handler 1");
-//     next();
-//   },
-// middlewares
-//   (req, res) => {
-//     // route handler 2
-//     res.send("Route handler 2");
-//     console.log("call Route handler 2");
-//   }
-// );
-
-app.get("/user", (req, res, next) => {
-  // route handler
-  // res.send("Route handler 1");
-  console.log("call Route handler 1");
-  next();
-});
-app.get("/user", (req, res, next) => {
-  // route handler
-  res.send("Route handler 1");
-  console.log("call Route handler 1");
-  next();
-});
