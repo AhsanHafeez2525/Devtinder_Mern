dd6fac85603f5c025ddf769b4ed5b414f6e9c583
AhsanHafeez2525 <ahsansatti402@gmail.com>
2025-09-14 10:05:03 -0700

    add: show user-connection-api

--- src/routes/user.js
+++ src/routes/user.js
@@ -8,7 +8,7 @@ const ConnectionRequest = require("../models/connectionRequest");
 const User = require("../models/user");
 // the fromUserId, the user is already login, so fromUserId comes from login token, it's same like when i swap
 // egt all the pending requests from the logged in user
-userRouter.get("/user/requests/recieved"), userAuth, async (req, res) => {
+userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
         const requests = await ConnectionRequest.find({ toUserId: loggedInUser._id, status: "interested" }).populate("fromUserId",["firstName","lastName", "photoUrl", "age", "gender", "about", "skills"]);
@@ -19,5 +19,17 @@ userRouter.get("/user/requests/recieved"), userAuth, async (req, res) => {
     } catch (error) {
         res.status(400).send("ERROR: " + error.message);
     }
-}
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const connections = await ConnectionRequest.find({ $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }] }).populate("fromUserId",["firstName","lastName", "photoUrl", "age", "gender", "about", "skills"]);
+        const data = connections.map((row) => row.fromUserId);
+        res.json({ data });
+    } catch (error) {
+        res.status(400).send("ERROR: " + error.message);
+    }
+});
+
 module.exports = userRouter;
