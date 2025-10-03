28f44183384e1638df2a93eea80b8634f0f47b6a
mac <mac@macs-MacBook-Pro.local>
2025-10-03 18:20:14 +0500

    add: implemented chat

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,11 +2,30 @@ const socketio = require("socket.io");
 const initializeSocket = (server) => {
     const io = socketio(server,{
       cors: {
-        origin: "http://localhost:5173",
+        origin: [
+          "http://localhost:3000",
+          "http://localhost:5173", 
+          "http://localhost:5174", 
+          "https://devtender.netlify.app"
+        ],
         credentials: true,
       },
     });
     io.on("connection", (socket) => {
+        // handle events
+        socket.on("join", ({firstName, lastName, userId, targetUserId}) => {
+            const roomId= [userId, targetUserId].sort().join("_");
+            console.log(firstName, lastName, "Joined room", roomId);
+            socket.join(roomId);
+        });
+        socket.on("sendMessage", ({ firstName, lastName, userId, targetUserId, text }) => {
+            const roomId = [userId, targetUserId].sort().join("_");
+            console.log(firstName, lastName, "Sent message", text);
+            io.to(roomId).emit("receiveMessage", { firstName, lastName, text });
+        });
+        socket.on("disconnect", () => {
+            console.log("Client disconnected");
+        });
       console.log("New client connected");
     });
 };
