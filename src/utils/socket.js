71059c982611d5827da107cd70ac3fb419a730a8
mac <mac@macs-MacBook-Pro.local>
2025-10-03 18:48:02 +0500

    add: crypto for roomid

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,9 @@
 const socketio = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (userId, targetUserId) => {
+    return crypto.createHash("sha256").update([userId, targetUserId].sort().join("_")).digest("hex");
+}
 const initializeSocket = (server) => {
     const io = socketio(server,{
       cors: {
@@ -12,14 +17,18 @@ const initializeSocket = (server) => {
       },
     });
     io.on("connection", (socket) => {
+        // Handle authentication
+        const token = socket.handshake.auth.token;
+        console.log("Client connected with token:", token);
+        
         // handle events
         socket.on("join", ({firstName, lastName, userId, targetUserId}) => {
-            const roomId= [userId, targetUserId].sort().join("_");
+            const roomId = getSecretRoomId(userId, targetUserId);
             console.log(firstName, lastName, "Joined room", roomId);
             socket.join(roomId);
         });
         socket.on("sendMessage", ({ firstName, lastName, userId, targetUserId, text }) => {
-            const roomId = [userId, targetUserId].sort().join("_");
+            const roomId = getSecretRoomId(userId, targetUserId);
             console.log(firstName, lastName, "Sent message", text);
             io.to(roomId).emit("receiveMessage", { firstName, lastName, text });
         });
